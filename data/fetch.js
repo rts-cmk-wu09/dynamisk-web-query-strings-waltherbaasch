0ff8a5ce932ff69f430bf29417ba37e39515e29c
waltherbaasch <U2444127@rts-365.dk>
2023-03-02 13:18:38 +0100

    added js

--- data/fetch.js
+++ data/fetch.js
@@ -1,12 +1,19 @@
-fetch('destinations.json')
-			.then(response => response.json())
-			.then(destinations => {
-				const destinationsList = document.getElementById('destinations-list');
 
-				destinations.forEach(destination => {
-					const li = document.createElement('li');
-					li.textContent = destination.title;
-					destinationsList.appendChild(li);
-				});
-			})
-			.catch(error => console.error(error));
+
+
+
+
+fetch('destinations.json')
+			.then((response) => {
+                return response.json();
+            })
+			.then(data => {
+				const destinationsList = document.getElementById('destinations-list');
+                console.log(data)
+			data.destinations.forEach(destination => {
+				const li = document.createElement('li');
+				li.textContent = destination.title;
+				destinationsList.appendChild(li);
+			});
+			});
+		
