44feccf01177342ecb8e40033e13fb251729d2bb
waltherbaasch <U2444127@rts-365.dk>
2023-03-03 09:09:23 +0100

    add js

---
+++ data/details.js
@@ -0,0 +1,4 @@
+const params = new URLSearchParams(document.location.search);
+const id = params.get("id");
+console.log(id);
+
---
+++ data/script.js
@@ -0,0 +1,1 @@
+document.querySelector(".destinationslist").addEventListener("click", (e) =>)
